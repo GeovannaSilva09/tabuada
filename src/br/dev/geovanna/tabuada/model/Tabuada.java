c021a098aee3f57f67a5961f5fa14edc4ed9ad65
GEOVANNA SILVA DE SOUSA <25132823@CFP127ALUNO.LOCAL>
2025-04-02 17:07:56 -0300

    Tabuada iniciada

---
+++ src/br/dev/geovanna/tabuada/model/Tabuada.java
@@ -0,0 +1,51 @@
+package br.dev.geovanna.tabuada.model;
+
+public class Tabuada {
+	
+	private double multiplicando;
+	private double minimoMultiplicador;
+	private double maximoMultiplicador;
+
+       
+	public void setMultiplicando (double multiplicando) {
+		this.multiplicando = multiplicando;
+	}
+	
+	public double getMultiplicando() {
+		return multiplicando;
+	}
+	
+	public void setMinimoMultiplicador (double minimoMultiplicador) {
+		this.minimoMultiplicador = minimoMultiplicador;
+	}
+
+	public double getMinimoMultiplicador() {
+		return minimoMultiplicador;
+	}
+	
+	public void setMaximoMultiplicador (double maximoMultiplicador) {
+		this.maximoMultiplicador = maximoMultiplicador;
+	}
+	
+	
+	public double getMaximoMultiplicador() {
+		return maximoMultiplicador;
+	}
+	 
+	public void mostrarTabuada() {
+		
+		double contador = minimoMultiplicador;
+		while (contador <= maximoMultiplicador) {
+			double produto = multiplicando * contador;
+			System.out.println(multiplicando + " x " + contador + " = " + produto);
+			    contador = contador + 1;
+			 
+		}
+		
+		System.out.println("---------------------------");
+		System.out.println("");
+		
+	}
+
+	
+}
